0f193fdb503c108bb4af675d9755e70c99135a8b
AskAfter <askafter904@gmail.com>
2025-02-19 09:14:28 +0100

    numbers section finish

--- src/components/Numbers/Numbers.jsx
+++ src/components/Numbers/Numbers.jsx
@@ -1,32 +1,35 @@
 import s from './Numbers.module.css';
+import rectangular from '../../assets/rectangleMobile.svg';
 
 const Numbers = () => {
   return (
     <div className={s.container}>
-      <h2 className={s.header}>Цифры</h2>
-      <p className={`${s.text} ${s.dateText}`}>Cентябрь 2022</p>
-      <div className={s.statsContainer}>
-        <div className={s.statColumn}>
-          <h3 className={`${s.text} ${s.numbersHeader}`}>
-            Торговой <br />
-            прибыли
-          </h3>
-          <p className={s.numbers}>2756%</p>
+      <img className={s.rectangular} src={rectangular} alt="rectangular" />
+      <div className={s.mainContent}>
+        <h2 className={s.header}>Цифры</h2>
+        <p className={`${s.text} ${s.dateText}`}>Cентябрь 2022</p>
+        <div className={s.statsContainer}>
+          <div className={s.statColumn}>
+            <h3 className={`${s.text} ${s.numbersHeader}`}>
+              Торговой <br />
+              прибыли
+            </h3>
+            <p className={s.numbers}>2756%</p>
+          </div>
+          <div className={s.statColumn}>
+            <h3 className={`${s.text} ${s.numbersHeader}`}>
+              фьючерсных <br />и спотовых сделок
+            </h3>
+            <p className={s.numbers}>67</p>
+          </div>
         </div>
-        <div className={s.statColumn}>
+        <div className={`${s.statColumn}`}>
           <h3 className={`${s.text} ${s.numbersHeader}`}>
-            фьючерсных <br />и спотовых сделок
+            прибыль <br />
+            подписчиков
           </h3>
-          <p className={s.numbers}>67</p>
+          <p className={s.numbers}>375000</p>
         </div>
-      </div>
-
-      <div className={`${s.statColumn}`}>
-        <h3 className={`${s.text} ${s.numbersHeader}`}>
-          прибыль <br />
-          подписчиков
-        </h3>
-        <p className={s.numbers}>375000</p>
       </div>
     </div>
   );
