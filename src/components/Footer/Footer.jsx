d0563daacb0a114dddc36c3f45bf664a4bbf81a6
AskAfter <askafter904@gmail.com>
2025-02-21 00:54:36 +0100

    footer adaptive

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,57 +4,61 @@ import tradeBladeIcon from '../../assets/tradeBlade.svg';
 const Footer = () => {
   return (
     <div className={s.container}>
-      <div className={s.topWrapper}>
-        <div className={s.navHeaderWrapper}>
-          <h3 className={s.navHeader}>Быстрая навигация</h3>
+      <div className={s.innerContainer}>
+        <div className={s.topWrapper}>
+          <div className={s.navHeaderWrapper}>
+            <h3 className={s.navHeader}>Быстрая навигация</h3>
+          </div>
+          <div>
+            <nav className={s.nav}>
+              <ul className={s.list}>
+                <li className={s.liLink}>
+                  <a href="#numbers" className={s.link}>
+                    Цифры
+                  </a>
+                </li>
+                <li className={s.liLink}>
+                  <a href="#deals" className={s.link}>
+                    Сделки онлайн
+                  </a>
+                </li>
+                <li className={s.liLink}>
+                  <a href="#about" className={s.link}>
+                    О компании
+                  </a>
+                </li>
+                <li className={s.liLink}>
+                  <a href="#tradeblad" className={s.link}>
+                    TradeBlade это
+                  </a>
+                </li>
+                <li>
+                  <a href="#how-to-start" className={`${s.link} ${s.hidden}`}>
+                    Как начать
+                  </a>
+                </li>
+                <li className={s.liLink}>
+                  <a href="#tariffs" className={s.link}>
+                    Тарифы
+                  </a>
+                </li>
+                <li>
+                  <a href="#faq" className={s.link}>
+                    FAQ
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
         </div>
-        <div>
-          <nav className={s.nav}>
-            <ul className={s.list}>
-              <li>
-                <a href="#numbers" className={s.link}>
-                  Цифры
-                </a>
-              </li>
-              <li>
-                <a href="#deals" className={s.link}>
-                  Сделки онлайн
-                </a>
-              </li>
-              <li>
-                <a href="#about" className={s.link}>
-                  О компании
-                </a>
-              </li>
-              <li>
-                <a href="#tradeblad" className={s.link}>
-                  TradeBlade это
-                </a>
-              </li>
-              <li>
-                <a href="#how-to-start" className={s.link}>
-                  Как начать
-                </a>
-              </li>
-              <li>
-                <a href="#tariffs" className={s.link}>
-                  Тарифы
-                </a>
-              </li>
-              <li>
-                <a href="#faq" className={s.link}>
-                  FAQ
-                </a>
-              </li>
-            </ul>
-          </nav>
+        <div className={s.bottomWrapper}>
+          <a href="#">
+            <img className={s.tradeBladeIcon} src={tradeBladeIcon} alt="Меню" />
+          </a>
+          <p className={s.allRights}>
+            &#xa9; 2022 TradeBlade. All rights reserved
+          </p>
         </div>
-      </div>
-      <div className={s.bottomWrapper}>
-        <a href="#">
-          <img className={s.tradeBladeIcon} src={tradeBladeIcon} alt="Меню" />
-        </a>
-        <p className={s.allRights}>© 2022 TradeBlade. All rights reserved</p>
       </div>
       <div className={s.box}></div>
     </div>
