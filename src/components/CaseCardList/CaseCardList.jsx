03212d54fb6032dc995d86fbeb020c3fb9fc4769
AskAfter <askafter904@gmail.com>
2025-02-20 21:11:27 +0100

    closed deals adaptive

--- src/components/CaseCardList/CaseCardList.jsx
+++ src/components/CaseCardList/CaseCardList.jsx
@@ -4,19 +4,37 @@ import 'swiper/css';
 import 'swiper/css/scrollbar';
 import CaseCard from '../CaseCard/CaseCard';
 import s from './CaseCardList.module.css';
+import { useEffect, useState } from 'react';
 
 const CaseCardList = ({ cases }) => {
+  const [spaceBetween, setSpaceBetween] = useState(8);
+  const [swiperWidth, setSwiperWidth] = useState(311);
   const slidesCount = cases.length;
-  const swiperWidth = 311;
   const dragWidth = swiperWidth / slidesCount;
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 1440) {
+        setSwiperWidth(955);
+        setSpaceBetween(20);
+      } else {
+        setSwiperWidth(311);
+        setSpaceBetween(8);
+      }
+    };
+    handleResize();
 
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
   return (
     <div className={s.swiperWrapper}>
       <Swiper
         modules={[Scrollbar]}
-        spaceBetween={8}
+        spaceBetween={spaceBetween}
         slidesPerView={'auto'}
         // loop={true}
+        // loopAdditionalSlides={10}
         scrollbar={{
           draggable: true,
           hide: false,
