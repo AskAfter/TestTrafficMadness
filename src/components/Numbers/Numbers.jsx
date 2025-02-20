f71e9e7132770817725d4e974f562b6872191b3e
AskAfter <askafter904@gmail.com>
2025-02-20 19:50:25 +0100

    third section

--- src/components/Numbers/Numbers.jsx
+++ src/components/Numbers/Numbers.jsx
@@ -4,29 +4,29 @@ import rectangular from '../../assets/rectangleMobile.svg';
 const Numbers = () => {
   return (
     <div className={s.container} id="numbers">
-      <img className={s.rectangular} src={rectangular} alt="rectangular" />
+      <div className={s.rectangular}></div>
       <div className={s.mainContent}>
-        <h2 className={s.header}>Цифры</h2>
-        <p className={`${s.text} ${s.dateText}`}>Cентябрь 2022</p>
+        <div className={s.headerWrap}>
+          <h2 className={s.header}>Цифры</h2>
+          <p className={`${s.text} ${s.dateText}`}>Cентябрь 2022</p>
+        </div>
         <div className={s.statsContainer}>
           <div className={s.statColumn}>
             <h3 className={`${s.text} ${s.numbersHeader}`}>
-              Торговой <br />
-              прибыли
+              Торговой <span className={s.lineBreak}>прибыли</span>
             </h3>
             <p className={s.numbers}>2756%</p>
           </div>
           <div className={s.statColumn}>
             <h3 className={`${s.text} ${s.numbersHeader}`}>
-              фьючерсных <br />и спотовых сделок
+              фьючерсных <span className={s.lineBreak}>и спотовых сделок</span>
             </h3>
             <p className={s.numbers}>67</p>
           </div>
         </div>
-        <div className={`${s.statColumn}`}>
+        <div className={`${s.statColumn} ${s.lastStatColumn}`}>
           <h3 className={`${s.text} ${s.numbersHeader}`}>
-            прибыль <br />
-            подписчиков
+            прибыль <span className={s.lineBreak}>подписчиков</span>
           </h3>
           <p className={s.numbers}>375000</p>
         </div>
