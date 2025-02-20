242f38118d853eac60ab551115d2faa3f9aa350f
AskAfter <askafter904@gmail.com>
2025-02-20 22:50:35 +0100

    tariffs adaptive

--- src/components/Tariffs/Tariffs.jsx
+++ src/components/Tariffs/Tariffs.jsx
@@ -83,120 +83,121 @@ const Tariffs = () => {
           фьючерс
         </button>
       </div>
-      <div className={`${s.standardCard} ${s.card}`}>
-        <h3 className={s.cardHeader}>STANDART</h3>
-        <ul className={s.cardTextThumb}>
-          <li className={s.benefitsThumb}>
-            <img className={s.icon} src={icon} alt="Иконка наличия" />
-            <p className={s.text}>Ручной трейдинг</p>
-          </li>
-          <img className={s.line} src={line} alt="Подчеркивание" />
-          <li className={s.benefitsThumb}>
-            <img className={s.icon} src={icon} alt="Иконка наличия" />
-            <p className={s.text}>
-              Автоматическое или полуавтоматическое копирование сделок
-            </p>
-          </li>
-          <img className={s.line} src={line} alt="Подчеркивание" />
-          <li className={s.benefitsThumb}>
-            <img className={s.icon} src={icon} alt="Иконка наличия" />
-            <p className={s.text}>Личный кабинет со статистикой</p>
-          </li>
-          <img className={s.line} src={line} alt="Подчеркивание" />
-          <li className={s.benefitsThumb}>
-            <img className={s.icon} src={icon} alt="Иконка наличия" />
-            <p className={s.text}>
-              Среднесрочные сделки с уровнями набора портфеля
-            </p>
-          </li>
-          <img className={s.line} src={line} alt="Подчеркивание" />
-        </ul>
-        <div className={s.priceTimeBox}>
-          <p className={s.price}>${price}</p>
-          <p className={s.discount}>{discount}%</p>
-          <select
-            className={s.select}
-            value={selectedOption}
-            onChange={handleSelectChange}
-          >
-            <option className={s.option} value="12 месяцев">
-              12 месяцев
-            </option>
-            <option className={s.option} value="6 месяцев">
-              6 месяцев
-            </option>
-            <option className={s.option} value="1 месяц">
-              1 месяц
-            </option>
-          </select>
+      <div className={s.cardsWrapper}>
+        <div className={`${s.standardCard} ${s.card}`}>
+          <h3 className={s.cardHeader}>STANDART</h3>
+          <ul className={s.cardTextThumb}>
+            <li className={s.benefitsThumb}>
+              <img className={s.icon} src={icon} alt="Иконка наличия" />
+              <p className={s.text}>Ручной трейдинг</p>
+            </li>
+            <img className={s.line} src={line} alt="Подчеркивание" />
+            <li className={s.benefitsThumb}>
+              <img className={s.icon} src={icon} alt="Иконка наличия" />
+              <p className={s.text}>
+                Автоматическое или полуавтоматическое копирование сделок
+              </p>
+            </li>
+            <img className={s.line} src={line} alt="Подчеркивание" />
+            <li className={s.benefitsThumb}>
+              <img className={s.icon} src={icon} alt="Иконка наличия" />
+              <p className={s.text}>Личный кабинет со статистикой</p>
+            </li>
+            <img className={s.line} src={line} alt="Подчеркивание" />
+            <li className={s.benefitsThumb}>
+              <img className={s.icon} src={icon} alt="Иконка наличия" />
+              <p className={s.text}>
+                Среднесрочные сделки с уровнями набора портфеля
+              </p>
+            </li>
+            <img className={s.line} src={line} alt="Подчеркивание" />
+          </ul>
+          <div className={s.priceTimeBox}>
+            <p className={s.price}>${price}</p>
+            <p className={s.discount}>{discount}%</p>
+            <select
+              className={s.select}
+              value={selectedOption}
+              onChange={handleSelectChange}
+            >
+              <option className={s.option} value="12 месяцев">
+                12 месяцев
+              </option>
+              <option className={s.option} value="6 месяцев">
+                6 месяцев
+              </option>
+              <option className={s.option} value="1 месяц">
+                1 месяц
+              </option>
+            </select>
+          </div>
+          <button className={s.tryBtn}>
+            <span className={s.lineOne}>Попробовать</span>
+            <span className={s.lineTwo}>5 дней бесплатно</span>
+          </button>
         </div>
-        <button className={s.tryBtn}>
-          <span className={s.lineOne}>Попробовать</span>
-          <span className={s.lineTwo}>5 дней бесплатно</span>
-        </button>
-      </div>
-
-      <div className={`${s.standardCard} ${s.vip}`}>
-        <h3 className={s.cardHeader}>VIP</h3>
-        <ul className={s.cardTextThumb}>
-          <li className={s.benefitsThumb}>
-            <img className={s.icon} src={icon} alt="Иконка наличия" />
-            <p className={s.text}>Ручной трейдинг</p>
-          </li>
-          <img className={s.line} src={line} alt="Подчеркивание" />
-          <li className={s.benefitsThumb}>
-            <img className={s.icon} src={icon} alt="Иконка наличия" />
-            <p className={s.text}>
-              Автоматическое или полуавтоматическое копирование сделок
-            </p>
-          </li>
-          <img className={s.line} src={line} alt="Подчеркивание" />
-          <li className={s.benefitsThumb}>
-            <img className={s.icon} src={icon} alt="Иконка наличия" />
-            <p className={s.text}>Личный кабинет со статистикой</p>
-          </li>
-          <img className={s.line} src={line} alt="Подчеркивание" />
-          <li className={s.benefitsThumb}>
-            <img className={s.icon} src={icon} alt="Иконка наличия" />
-            <p className={s.text}>
-              Краткосрочные, среднесрочные и инвест сделки
-            </p>
-          </li>
-          <img className={s.line} src={line} alt="Подчеркивание" />
-          <li className={s.benefitsThumb}>
-            <img className={s.icon} src={icon} alt="Иконка наличия" />
-            <p className={s.text}>Доступ в Vip чат с командой</p>
-          </li>
-          <img className={s.line} src={line} alt="Подчеркивание" />
-          <li className={s.benefitsThumb}>
-            <img className={s.icon} src={icon} alt="Иконка наличия" />
-            <p className={s.text}>Наш авторский курс по трейдингу</p>
-          </li>
-          <img className={s.line} src={line} alt="Подчеркивание" />
-        </ul>
-        <div className={s.priceTimeBox}>
-          <p className={s.price}>${vipPrice}</p>
-          <p className={s.discount}>{vipDiscount}%</p>
-          <select
-            className={s.select}
-            value={selectedOptionVIP}
-            onChange={handleSelectVipChange}
-          >
-            <option className={s.option} value="12 месяцев">
-              12 месяцев
-            </option>
-            <option className={s.option} value="6 месяцев">
-              6 месяцев
-            </option>
-            <option className={s.option} value="1 месяц">
-              1 месяц
-            </option>
-          </select>
+        <div className={`${s.standardCard} ${s.vip}`}>
+          <h3 className={s.cardHeader}>VIP</h3>
+          <ul className={s.cardTextThumb}>
+            <li className={s.benefitsThumb}>
+              <img className={s.icon} src={icon} alt="Иконка наличия" />
+              <p className={s.text}>Ручной трейдинг</p>
+            </li>
+            <img className={s.line} src={line} alt="Подчеркивание" />
+            <li className={s.benefitsThumb}>
+              <img className={s.icon} src={icon} alt="Иконка наличия" />
+              <p className={s.text}>
+                Автоматическое или полуавтоматическое копирование сделок
+              </p>
+            </li>
+            <img className={s.line} src={line} alt="Подчеркивание" />
+            <li className={s.benefitsThumb}>
+              <img className={s.icon} src={icon} alt="Иконка наличия" />
+              <p className={s.text}>Личный кабинет со статистикой</p>
+            </li>
+            <img className={s.line} src={line} alt="Подчеркивание" />
+            <li className={s.benefitsThumb}>
+              <img className={s.icon} src={icon} alt="Иконка наличия" />
+              <p className={s.text}>
+                Краткосрочные, среднесрочные и инвест сделки
+              </p>
+            </li>
+            <img className={s.line} src={line} alt="Подчеркивание" />
+            <li className={s.benefitsThumb}>
+              <img className={s.icon} src={icon} alt="Иконка наличия" />
+              <p className={s.text}>Доступ в Vip чат с командой</p>
+            </li>
+            <img className={s.line} src={line} alt="Подчеркивание" />
+            <li className={s.benefitsThumb}>
+              <img className={s.icon} src={icon} alt="Иконка наличия" />
+              <p className={s.text}>Наш авторский курс по трейдингу</p>
+            </li>
+            <img className={s.line} src={line} alt="Подчеркивание" />
+          </ul>
+          <div className={s.priceTimeBox}>
+            <p className={s.price}>${vipPrice}</p>
+            <p className={s.discount}>{vipDiscount}%</p>
+            <select
+              className={s.select}
+              value={selectedOptionVIP}
+              onChange={handleSelectVipChange}
+            >
+              <option className={s.option} value="12 месяцев">
+                12 месяцев
+              </option>
+              <option className={s.option} value="6 месяцев">
+                6 месяцев
+              </option>
+              <option className={s.option} value="1 месяц">
+                1 месяц
+              </option>
+            </select>
+          </div>
+          <button className={s.tryBtn}>
+            <span className={s.lineOne}>Попробовать</span>
+            <span className={s.lineTwo}>5 дней бесплатно</span>
+          </button>
         </div>
-        <button className={s.tryBtn}>
-          <span className={s.lineOne}>Попробовать</span>
-          <span className={s.lineTwo}>5 дней бесплатно</span>
-        </button>
       </div>
     </div>
   );
