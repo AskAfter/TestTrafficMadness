8f81181b13f7283eb0c58a3cd087869299f7c530
AskAfter <askafter904@gmail.com>
2025-02-24 02:11:23 +0100

    rectangular one more

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,31 +2,32 @@ import statistic from './statistic.svg';
 import s from './Hero.module.css';
 const Hero = () => {
   return (
-    <div className={s.container}>
-      <img src={statistic} alt="Цена акций" className={s.pricePicture} />
-      <h1 className={s.header}>моментально Копируй сделки профи трейдеров</h1>
-      <p className={s.text}>
-        Начни копировать сделки с успешной командой профессиональных трейдеров в
-        автоматическом режиме.
-      </p>
-
-      <div className={s.emailForm}>
-        <form className={s.emailInputContainer}>
-          <input
-            type="email"
-            placeholder="ВАШ E-MAIL"
-            //   value={email}
-            //   onChange={onEmailChange}
-            className={`${s.emailInput} buttonText`}
-          />
-          <button
-            //   onClick={onSubmit}
-            className={`${s.submitButton} buttonText`}
-          >
-            начать
-          </button>
-        </form>
-        <p className={s.freeTrialText}>5 дней бесплатного пользования</p>
+    <div>
+      <div className={s.container}>
+        <img src={statistic} alt="Цена акций" className={s.pricePicture} />
+        <h1 className={s.header}>моментально Копируй сделки профи трейдеров</h1>
+        <p className={s.text}>
+          Начни копировать сделки с успешной командой профессиональных трейдеров
+          в автоматическом режиме.
+        </p>
+        <div className={s.emailForm}>
+          <form className={s.emailInputContainer}>
+            <input
+              type="email"
+              placeholder="ВАШ E-MAIL"
+              //   value={email}
+              //   onChange={onEmailChange}
+              className={`${s.emailInput} buttonText`}
+            />
+            <button
+              //   onClick={onSubmit}
+              className={`${s.submitButton} buttonText`}
+            >
+              начать
+            </button>
+          </form>
+          <p className={s.freeTrialText}>5 дней бесплатного пользования</p>
+        </div>
       </div>
     </div>
   );
